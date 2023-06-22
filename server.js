3e039e446829b1bc33af171a82312ed035262484
vikash-A0 <thisisvikash75@gmail.com>
2023-06-22 09:23:43 +0530

    express

---
+++ server.js
@@ -0,0 +1,26 @@
+/*console.log("Hello");
+
+const express = require("express");
+const app = express();
+
+app.listen(3000); --- if we give like this it shows { Cannot GET /} in web browser, we need to mention routes to get proper op*/
+
+const express = require("express");
+const req = require("express/lib/request");
+const res = require("express/lib/response");
+const app = express();
+
+app.set("view engine", "ejs"); // to overcome error in 19th line
+
+app.get("/", (req, res) => {
+  console.log("Hello Nandhini");
+  //res.sendStatus(500); -- for send status
+  //res.status(500).send("HI"); -- for sending msg while the status of the code display
+  //res.download("server.js"); -- to download the file in this folder
+  //res.render("index"); //-- it shows an error as No default engine was specified
+  res.render("index", { text: "World" }); // to give data to html page
+});
+
+const userRouter = require("./routes/users");
+app.use("/users", userRouter);
+app.listen(3000);
